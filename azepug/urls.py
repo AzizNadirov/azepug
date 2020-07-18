d8685db3b8044d698435a81b5065680e797a9f2b
Aziz Nadirov <eziznadirov@gmail.com>
2020-07-18 16:40:36 +0400

    login/logout system

--- azepug/urls.py
+++ azepug/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from users import views as users_views
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('register/', include('users.urls')),
+    path('login/',auth_views.LoginView.as_view(template_name = 'users/login.html'), name = "login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = "logout"),
+    path('profile/', users_views.profile, name = 'profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .forms import UserRegistrationForm
 
@@ -9,8 +10,12 @@ def register(request):
         if form.is_valid():
             form.save() 
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Qeydiyyat uğurla tamamlandı. Xoş gəldiniz {username}!')
-            return redirect('home:Home')
+            messages.success(request, f'Qeydiyyat uğurla tamamlandı. Zəhmət olmasa yenidən daxil olun {username}!')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request,'users/register.html',{'form': form} )
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
