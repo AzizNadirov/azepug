18270b78a1487f62eeefd3bc52757cdec511bbb4
Aziz Nadirov <eziznadirov@gmail.com>
2020-07-22 11:59:50 +0400

    add:user profile edit

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
-from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -17,5 +17,26 @@ def register(request):
     return render(request,'users/register.html',{'form': form} )
 
 @login_required
+def edit_profile_view(request):
+    if request.method == 'POST':
+        print("in first if---------------------------")
+        u_form = UserUpdateForm(request.POST, request.FILES,  instance = request.user)
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+        print(u_form.is_valid() and p_form.is_valid())
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            print("*" * 20)
+            messages.success(request, f'Dəyişiklik uğurla tamamlandı.')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+    context = {'u_form':u_form,'p_form':p_form}
+    return render(request, 'users/edit_profile.html', context)
+
 def profile(request):
     return render(request, 'users/profile.html')
