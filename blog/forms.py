484bda1269e38171ed710eea9d129bb602685e2f
AzizNadirov <eziznadirov@gmail.com>
2022-02-06 23:08:01 +0300

    ...

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.forms import fields
 from .models import Comment, Post
+from taggit.models import Tag
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -9,7 +9,9 @@ class CommentForm(forms.ModelForm):
 
 
 class CreatePostForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(queryset= Tag.objects.all(), label='Tag', widget=forms.TextInput(attrs={'class': "form-control"}))
     class Meta:
         model = Post
         fields = ['title', 'content', 'drafted', 'tags']
-        # wingets = {}
+        # wingets = {'tags': forms.TextInput(attrs={'class': 'form-control'})}
+        
