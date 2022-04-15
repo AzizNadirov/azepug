69cb2e34ddfb57c5a3c276ef41466ac7f5e3ed31
AzizNadirov <eziznadirov@gmail.com>
2022-04-15 21:13:47 +0400

    .

--- azepug/settings.py
+++ azepug/settings.py
@@ -116,9 +116,13 @@ USE_TZ = True
 
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 MEDIA_URL = '/media/'
